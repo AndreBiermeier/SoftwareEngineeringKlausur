1427b46a4a7afcb83ebf3ae99d1fcddc466a8926
AndreBiermeier <165042098+AndreBiermeier@users.noreply.github.com>
2025-02-10 17:44:56 +0100

    preparation for UnitTests

---
+++ src/BoardTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BoardTest {
+
+    @Test
+    public void testConstructor() {
+        int a = 5;
+        assertNotNull( a );
+    }
+
+    @Test
+    public void testFunctionName(){
+        boolean var = true;
+        assertEquals(var,false);
+    }
+}
